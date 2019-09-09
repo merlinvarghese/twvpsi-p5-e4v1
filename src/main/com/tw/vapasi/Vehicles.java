40b56a66d7af87f0655033b171a7b141944b0f88
Archana Panchal <panchalarchana12@gmail.com>
2019-09-09 19:22:26 +0530

    add . Implemented owner functionality

---
+++ src/main/com/tw/vapasi/Vehicles.java
@@ -0,0 +1,14 @@
+package com.tw.vapasi;
+
+
+public class Vehicle implements Parkable {
+    final String registrationNumber;
+
+    public Vehicle(String name) {
+        this.registrationNumber = name;
+    }
+
+    public String getName() {
+        return registrationNumber;
+    }
+}
---
+++ src/test/com/tw/vapasi/OwnerTest.java
@@ -0,0 +1,7 @@
+package com.tw.vapasi;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+}
